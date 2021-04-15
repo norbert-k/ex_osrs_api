87e154ae9cb412da5172eda22d49e69ce3577d3a
nka <kakste.norberts@gmail.com>
2021-04-16 01:32:08 +0300

    Removed package name

--- mix.exs
+++ mix.exs
@@ -45,11 +45,7 @@ defmodule ExOsrsApi.MixProject do
 
   defp package() do
     [
-      # This option is only needed when you don't want to use the OTP application name
-      name: "ExOsrsApi",
-      # These are the default files included in the package
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-                license* src),
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/norbert-k/ex_osrs_api"}
     ]
